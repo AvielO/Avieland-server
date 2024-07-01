0d2a2e5037735914c5bbf44b536584940a4d5cc8
Aviel <aviell1100611@gmail.com>
2024-07-02 02:33:46 +0300

    Unnecessary line deleted

--- app/usersActions.js
+++ app/usersActions.js
@@ -118,7 +118,6 @@ export const attackUser = async (attackerUsername, targetUsername) => {
   } else {
     report = new Report({
       id: reportID,
-      
       attacker: attackerUserReport,
       defender: defenderUserReport,
       winner: "defender",
