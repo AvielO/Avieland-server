59e43af6865f619cfbb64a363a1eecbe5a0bfcbd
Aviel <aviell1100611@gmail.com>
2024-07-05 20:24:05 +0300

    When buy workers the resources decrease

--- app/workerActions.js
+++ app/workerActions.js
@@ -1,5 +1,9 @@
 import { getUserByUsername } from "../db/users.js";
 
+const COPPER_WORKER_COST = 100;
+const SILVER_WORKER_COST = 100;
+const GOLD_WORKER_COST = 100;
+
 export const hireWorkers = async (
   username,
   copperWorkersQuantity,
@@ -11,15 +15,33 @@ export const hireWorkers = async (
     isNaN(silverWorkersQuantity) ||
     isNaN(goldWorkersQuantity)
   ) {
-    console.log("H")
+    console.log("H");
     throw new Error("Not a number");
   }
   const user = await getUserByUsername(username);
   if (!user) return;
 
-  user.workers.copper += +copperWorkersQuantity;
-  user.workers.silver += +silverWorkersQuantity;
-  user.workers.gold += +goldWorkersQuantity;
+  const copperCost = copperWorkersQuantity * COPPER_WORKER_COST;
+  const silverCost = silverWorkersQuantity * SILVER_WORKER_COST;
+  const goldCost = goldWorkersQuantity * GOLD_WORKER_COST;
 
-  await user.save();
+  const { copper, silver, gold } = user.resources;
+
+  const copperRemaining = copper - copperCost;
+  const silverRemaining = silver - silverCost;
+  const goldRemaining = gold - goldCost;
+
+  if (copperRemaining >= 0 && silverRemaining >= 0 && goldRemaining >= 0) {
+    user.resources.copper = copperRemaining;
+    user.resources.silver = silverRemaining;
+    user.resources.gold = goldRemaining;
+
+    user.workers.copper += +copperWorkersQuantity;
+    user.workers.silver += +silverWorkersQuantity;
+    user.workers.gold += +goldWorkersQuantity;
+
+    await user.save();
+  } else {
+    throw new Error("Have no enough resources");
+  }
 };
